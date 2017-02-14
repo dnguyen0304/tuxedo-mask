bdbb48721586bb65a0aacb564e09a98eebcc7373
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-14 07:24:34 -0500

    ME-448: Removed encode_credentials()
    It had only 1 reference.

--- tuxedo_mask/tests/test_services.py
+++ tuxedo_mask/tests/test_services.py
@@ -7,17 +7,10 @@ from nose.tools import assert_tuple_equal
 from tuxedo_mask import services
 
 
-def encode_credentials(username, password):
-
-    credentials = username + ':' + password
-    encoded = base64.b64encode(credentials.encode('utf-8'))
-    return encoded
-
-
 def test_decode_credentials():
 
     credentials = ('foo', 'bar')
-    encoded = encode_credentials(*credentials)
+    encoded = base64.b64encode(':'.join(credentials).encode('utf-8'))
     decoded = services.decode_credentials(encoded)
     assert_tuple_equal(decoded, credentials)
 
