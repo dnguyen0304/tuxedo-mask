033352cf306989f4ccc87dc16c445473634ad2cb
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-15 17:01:07 -0500

    ME-450: Renamed sub-package to "repositories"

--- tuxedo_mask/repositories/tests/test_unit_of_work.py
+++ tuxedo_mask/repositories/tests/test_unit_of_work.py
@@ -2,7 +2,7 @@
 
 from nose.tools import assert_is_instance, assert_true
 
-from tuxedo_mask import authentication
+from tuxedo_mask import repositories
 
 
 def test_unit_of_work_has_repository_attributes():
@@ -17,13 +17,13 @@ def test_unit_of_work_has_repository_attributes():
     class BarRepository(BaseRepository):
         pass
 
-    repositories = {'foo_repository': FooRepository,
-                    'bar_repository': BarRepository}
-    unit_of_work = authentication.UnitOfWork(repositories=repositories,
-                                             db_context=None,
-                                             logger=None)
+    repositories_ = {'foo_repository': FooRepository,
+                     'bar_repository': BarRepository}
+    unit_of_work = repositories.UnitOfWork(repositories=repositories_,
+                                           db_context=None,
+                                           logger=None)
 
-    for name, class_ in repositories.items():
+    for name, class_ in repositories_.items():
         assert_true(hasattr(unit_of_work, '_' + name))
         assert_is_instance(getattr(unit_of_work, '_' + name), class_)
 
