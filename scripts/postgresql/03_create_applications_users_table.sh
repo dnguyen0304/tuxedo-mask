28f857ad2c8708ea181e8432367712ac8a484c37
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-15 16:33:21 -0500

    ME-450: Migrated to flat model naming convention

--- scripts/postgresql/03_create_applications_users_table.sh
+++ scripts/postgresql/03_create_applications_users_table.sh
@@ -5,17 +5,17 @@ set -e
 tuxedo_mask_environment=${TUXEDO_MASK_ENVIRONMENT,,}
 
 psql --set ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "tuxedo_mask_$tuxedo_mask_environment" <<EOF
-    DROP TABLE IF EXISTS applications_users;
+    DROP TABLE IF EXISTS users;
 
-    CREATE TABLE applications_users (
-        applications_users_id     serial                      PRIMARY KEY,
-        applications_users_uuid   uuid            NOT NULL    DEFAULT uuid_generate_v1mc()                  UNIQUE,
-        applications_id           int             NOT NULL    REFERENCES applications (applications_id),
-        username                  varchar(32)     NOT NULL,
-        password                  varchar(255)    NOT NULL,
-        created_at                timestamp       NOT NULL    DEFAULT CURRENT_TIMESTAMP,
-        created_by                int             NOT NULL,
-        updated_at                timestamp,
-        updated_by                int
+    CREATE TABLE users (
+        users_id        serial                      PRIMARY KEY,
+        users_uuid      uuid            NOT NULL    DEFAULT uuid_generate_v1mc()                  UNIQUE,
+        applications_id int             NOT NULL    REFERENCES applications (applications_id),
+        username        varchar(32)     NOT NULL,
+        password        varchar(255)    NOT NULL,
+        created_at      timestamp       NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+        created_by      int             NOT NULL,
+        updated_at      timestamp,
+        updated_by      int
     );
 EOF
