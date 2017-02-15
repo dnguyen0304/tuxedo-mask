3b489c9943ee1b7527132f9af05481c47fba77de
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-15 18:48:59 -0500

    ME-450: Migrated tables to string IDs
    from UUIDs

--- scripts/postgresql/90_initialize_tuxedo_mask.sh
+++ scripts/postgresql/90_initialize_tuxedo_mask.sh
@@ -3,12 +3,14 @@
 set -e
 
 tuxedo_mask_environment=${TUXEDO_MASK_ENVIRONMENT,,}
+applications_sid=$(cat /dev/urandom | tr -cd '0-9A-Za-z' | head -c 32)
 
 psql --set ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "tuxedo_mask_$tuxedo_mask_environment" <<EOF
     INSERT INTO applications (
+        applications_sid,
         name,
         created_by
     )
     VALUES
-        ('tuxedo_mask', 1);
+        ('$applications_sid', 'tuxedo_mask', 1);
 EOF
