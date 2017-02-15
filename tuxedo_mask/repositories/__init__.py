a9639757d04d2100d245c1d1ba2b429cefa7e4c0
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-15 18:14:35 -0500

    ME-450: Added TuxedoMaskBaseRepository

--- tuxedo_mask/repositories/__init__.py
+++ tuxedo_mask/repositories/__init__.py
@@ -2,6 +2,10 @@
 
 from .base_repository import EntityNotFound, BaseRepository
 from .unit_of_work import UnitOfWork
+from .tuxedo_mask_base_repository import TuxedoMaskBaseRepository
 
-__all__ = ['BaseRepository', 'EntityNotFound', 'UnitOfWork']
+__all__ = ['BaseRepository',
+           'EntityNotFound',
+           'TuxedoMaskBaseRepository',
+           'UnitOfWork']
 
