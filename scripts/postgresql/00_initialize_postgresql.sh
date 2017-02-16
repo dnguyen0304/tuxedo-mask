5715c4d9f9513939b3d01488449987191743166d
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-16 05:12:06 -0500

    ME-450: Fixed not being able to log in

--- scripts/postgresql/00_initialize_postgresql.sh
+++ scripts/postgresql/00_initialize_postgresql.sh
@@ -5,9 +5,5 @@ set -e
 tuxedo_mask_environment=${TUXEDO_MASK_ENVIRONMENT,,}
 
 psql --set ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOF
-    CREATE ROLE tuxedo_mask_$tuxedo_mask_environment;
     CREATE DATABASE tuxedo_mask_$tuxedo_mask_environment;
-
-    GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO tuxedo_mask_$tuxedo_mask_environment;
-    GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO tuxedo_mask_$tuxedo_mask_environment;
 EOF
