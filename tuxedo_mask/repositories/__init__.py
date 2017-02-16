9978539567a10f01d7d8bd2366652faa8c341ebb
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-16 08:07:13 -0500

    ME-450: Added repositories.EntityConflict

--- tuxedo_mask/repositories/__init__.py
+++ tuxedo_mask/repositories/__init__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from .base_repository import EntityNotFound, BaseRepository
+from .base_repository import EntityConflict, EntityNotFound, BaseRepository
 from .unit_of_work import UnitOfWork
 from .tuxedo_mask_base_repository import TuxedoMaskBaseRepository
 from .tuxedo_mask_applications_repository import TuxedoMaskApplicationsRepository
 from .tuxedo_mask_users_repository import TuxedoMaskUsersRepository
 
 __all__ = ['BaseRepository',
+           'EntityConflict',
            'EntityNotFound',
            'TuxedoMaskApplicationsRepository',
            'TuxedoMaskBaseRepository',
