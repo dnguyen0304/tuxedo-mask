268a392b74f0531acec6287661a2daf52384f5f0
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-16 05:25:34 -0500

    ME-450: Added comment

--- tuxedo_mask/models/applications.py
+++ tuxedo_mask/models/applications.py
@@ -14,6 +14,7 @@ class Applications(Base):
     applications_sid = Column()
     name = Column()
 
+    # Should this feature be removed?
     users = relationship('Users')
 
     def __init__(self, name):
