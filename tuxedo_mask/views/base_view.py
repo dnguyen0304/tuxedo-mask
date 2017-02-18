7f1c26b6b25219c32b0d1561925cfa3c3050dbd4
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-18 09:58:21 -0500

    ME-441: Added BaseView
    Added marshmallow dependency

---
+++ tuxedo_mask/views/base_view.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import marshmallow
+from marshmallow import fields
+
+from tuxedo_mask import models
+
+
+class BaseView(marshmallow.Schema):
+
+    class Meta:
+        dateformat = 'iso'
+        ordered = True
+
+    created_at = fields.DateTime(dump_only=True)
+    created_by = fields.String(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
+    updated_by = fields.String(dump_only=True)
+
+    @marshmallow.post_load
+    def _deserialize(self, data):
+        return models.Base(**data)
+
