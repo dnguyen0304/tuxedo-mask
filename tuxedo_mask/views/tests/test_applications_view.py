a3a2d46ef9cec73cab9225b77d619e5993b1ed92
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-19 08:42:54 -0500

    ME-441: Renamed tests for consistency

--- tuxedo_mask/views/tests/test_applications_view.py
+++ tuxedo_mask/views/tests/test_applications_view.py
@@ -27,11 +27,11 @@ class TestApplicationsView(BaseTestCase):
     def set_up_serialization_fails_silently(self):
         self.unmarshall_with(update={'name': ''})
 
-    def test_name_minimum_length(self):
+    def test_deserialization_name_minimum_length(self):
         self.unmarshall_with(update={'name': ''})
         assert_in('name', self.unmarshalled_result.errors)
 
-    def test_name_maximum_length(self):
+    def test_deserialization_name_maximum_length(self):
         self.unmarshall_with(update={'name': 'x' * 50})
         assert_in('name', self.unmarshalled_result.errors)
 
