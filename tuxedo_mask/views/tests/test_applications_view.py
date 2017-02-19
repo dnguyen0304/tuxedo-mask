89ab8996ef0f2b0eeff2e5ecff625f7e7bd671df
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-19 07:06:27 -0500

    ME-441: Added tests

--- tuxedo_mask/views/tests/test_applications_view.py
+++ tuxedo_mask/views/tests/test_applications_view.py
@@ -25,7 +25,11 @@ class TestApplicationsView(BaseTestCase):
         return ['foo', 'foo']
 
     def set_up_serialization_fails_silently(self):
-        pass
+        self.unmarshall_with(update={'name': ''})
+
+    def test_name_minimum_length(self):
+        self.unmarshall_with(update={'name': ''})
+        assert_in('name', self.unmarshalled_result.errors)
 
     def test_name_maximum_length(self):
         self.unmarshall_with(update={'name': 'x' * 50})
