e20f4433b536ef19466bfcfbafe0111b16618b02
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-19 09:15:37 -0500

    ME-441: Added UsersView

--- tuxedo_mask/views/tests/test_users_view.py
+++ tuxedo_mask/views/tests/test_users_view.py
@@ -5,6 +5,8 @@ import re
 import marshmallow
 from nose.tools import assert_equal, assert_in, assert_true, raises
 
+from . import BaseTestCase
+from tuxedo_mask import models, views
 from tuxedo_mask.views import users_view
 
 
@@ -35,4 +37,50 @@ class TestContainsAtLeast:
         assert_equal(result.groups()[0], value)
         assert_in(choices[0], result.groups()[1])
         assert_equal(result.groups()[2], '1')
+
+
+class TestUsersView(BaseTestCase):
+
+    @property
+    def _Model(self):
+        return models.Users
+
+    @property
+    def _View(self):
+        return views.UsersView
+
+    @property
+    def fields(self):
+        return ['users_sid', 'username', 'password']
+
+    @property
+    def values(self):
+        return ['foo', 'foo', 'Foo45678']
+
+    def set_up_serialization_fails_silently(self):
+        pass
+
+    def test_username_minimum_length(self):
+        self.unmarshall_with(update={'username': str()})
+        assert_in('username', self.unmarshalled_result.errors)
+
+    def test_username_maximum_length(self):
+        self.unmarshall_with(update={'username': 'x' * 50})
+        assert_in('username', self.unmarshalled_result.errors)
+
+    def test_password_minimum_length(self):
+        self.unmarshall_with(update={'password': 'Foo4567'})
+        assert_in('password', self.unmarshalled_result.errors)
+
+    def test_password_contains_lowercase_characters(self):
+        self.unmarshall_with(update={'password': 'FOO45678'})
+        assert_in('password', self.unmarshalled_result.errors)
+
+    def test_password_contains_uppercase_characters(self):
+        self.unmarshall_with(update={'password': 'foo45678'})
+        assert_in('password', self.unmarshalled_result.errors)
+
+    def test_password_contains_numeric_characters(self):
+        self.unmarshall_with(update={'password': 'Fooooooo'})
+        assert_in('password', self.unmarshalled_result.errors)
 
--- tuxedo_mask/views/users_view.py
+++ tuxedo_mask/views/users_view.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import string
+
 import marshmallow
-from marshmallow import validate
+from marshmallow import fields, validate
+
+from . import BaseView
+from tuxedo_mask import models
 
 
 class ContainsAtLeast(validate.Validator):
@@ -53,4 +58,51 @@ class ContainsAtLeast(validate.Validator):
             message = self._format_error(value=value)
             raise marshmallow.exceptions.ValidationError(message)
         return value
+
+
+error_messages = {
+    'username': {
+        'length': """The username must be between {min} and {max} """
+                  """characters in length, inclusive. You specified """
+                  """"{input}"."""
+    },
+    'password': {
+        'minimum_length': """The password must be at least {min} """
+                          """characters in length.""",
+        'lowercase_characters': """The password must contain at least """
+                                """{count} lowercase character.""",
+        'uppercase_characters': """The password must contain at least """
+                                """{count} uppercase character.""",
+        'numeric_characters': """The password must contain at least """
+                              """{count} numeric character."""
+    }
+}
+
+
+class _UsersView(marshmallow.Schema):
+
+    users_sid = fields.String(dump_only=True)
+    username = fields.String(
+        required=True,
+        validate=[
+            validate.Length(min=1,
+                            max=32,
+                            error=error_messages['username']['length'])])
+    password = fields.String(
+        required=True,
+        validate=[
+            validate.Length(min=8,
+                            error=error_messages['password']['minimum_length']),
+            ContainsAtLeast(choices=string.ascii_lowercase,
+                            error=error_messages['password']['lowercase_characters']),
+            ContainsAtLeast(choices=string.ascii_uppercase,
+                            error=error_messages['password']['uppercase_characters']),
+            ContainsAtLeast(choices=string.digits,
+                            error=error_messages['password']['numeric_characters'])],
+        load_only=True)
+
+
+class UsersView(BaseView, _UsersView):
+
+    _Model = models.Users
 
