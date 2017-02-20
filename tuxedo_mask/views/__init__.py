06c076b524d83bc441e854f9e86171163523eead
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-20 07:24:55 -0500

    ME-441: Fixed import order

--- tuxedo_mask/views/__init__.py
+++ tuxedo_mask/views/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from .base_view import BaseView
-from .applications_view import ApplicationsView
 from .users_view import UsersView
+from .applications_view import ApplicationsView
 
 __all__ = ['ApplicationsView', 'BaseView', 'UsersView']
 
