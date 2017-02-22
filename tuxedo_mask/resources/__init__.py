2d87aa019203c662a394977872477e79e22b9a83
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-22 15:52:03 -0500

    ME-441: Added UsersCollectionResource stub with authentication
    Added Flask-HTTPAuth dependency

--- tuxedo_mask/resources/__init__.py
+++ tuxedo_mask/resources/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from .authentication import authentication
 from .applications_resource import ApplicationsCollectionResource
+from .users_resource import UsersCollectionResource
 
-__all__ = ['ApplicationsCollectionResource']
+__all__ = ['ApplicationsCollectionResource',
+           'UsersCollectionResource',
+           'authentication']
 
