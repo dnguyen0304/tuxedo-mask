3c553dbbb943932d456bd8e7b36571685daa54c4
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-22 18:28:53 -0500

    ME-441: Migrated to request decorators

--- tuxedo_mask/api.py
+++ tuxedo_mask/api.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import logging
+
 import flask
 import flask_restful
 
-from tuxedo_mask import resources
+from tuxedo_mask import resources, services
 
 app = flask.Flask(__name__)
 api = flask_restful.Api(app=app)
 
 api.add_resource(resources.ApplicationsCollectionResource, '/v1/applications/')
 api.add_resource(resources.UsersCollectionResource, '/v1/applications/<string:applications_sid>/users/')
+
+
+@app.before_request
+def do_before_request():
+    flask.g.logger = logging.getLogger(name='tuxedo_mask')
+    flask.g.service = services.TuxedoMaskService.from_configuration()
+
+
+@app.after_request
+def do_after_request(response):
+    flask.g.service.dispose()
+
+    return response
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
