409c5da791dd9524796ba2895d2fc46b312b85b9
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-22 11:09:47 -0500

    N/A: Renamed clients to services

--- tuxedo_mask/services/__init__.py
+++ tuxedo_mask/services/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .base_client import BaseClient
+from .base_service import BaseClient
 from .tuxedo_mask_service import TuxedoMaskClient
 
 __all__ = ['BaseClient', 'TuxedoMaskClient']
