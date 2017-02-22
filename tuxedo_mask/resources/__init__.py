63368fa8612d846e29419a17b0131e4f4fd8434b
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-22 15:51:13 -0500

    Reverted "ME-441: Added UsersCollectionResource stub with authentication"
    This reverts commit 6cfd86c2a8f8969e80c9ff0f9faeb83c99f0c036.

--- tuxedo_mask/resources/__init__.py
+++ tuxedo_mask/resources/__init__.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .authentication import authentication
 from .applications_resource import ApplicationsCollectionResource
-from .users_resource import UsersCollectionResource
 
-__all__ = ['ApplicationsCollectionResource',
-           'UsersCollectionResource',
-           'authentication']
+__all__ = ['ApplicationsCollectionResource']
 
