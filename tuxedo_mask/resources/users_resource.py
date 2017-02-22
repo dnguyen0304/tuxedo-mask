eb465bf14606a3e25d6b525225595a5f56141591
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-22 18:28:53 -0500

    ME-441: Migrated to request decorators

--- tuxedo_mask/resources/users_resource.py
+++ tuxedo_mask/resources/users_resource.py
@@ -2,21 +2,16 @@
 
 import collections
 import http
-import logging
 
 import flask
 import flask_restful
 import marshmallow
 
 from . import authentication
-from tuxedo_mask import repositories, views, services
+from tuxedo_mask import repositories, views
 
 
 class UsersCollectionResource(flask_restful.Resource):
-
-    def __init__(self):
-        self.logger = logging.getLogger('tuxedo_mask')
-        self.service = services.TuxedoMaskService.from_configuration()
 
     @authentication.login_required
     def post(self, applications_sid):
@@ -30,25 +25,23 @@ class UsersCollectionResource(flask_restful.Resource):
             user = views.UsersView().load(data=flask.request.get_json()).data
         except marshmallow.exceptions.ValidationError as e:
             http_status_code = http.HTTPStatus.BAD_REQUEST
-            self.logger.info(e.messages)
+            flask.g.logger.info(e.messages)
             return body, http_status_code, headers
 
-        application = self.service.applications.get_by_sid(applications_sid)
+        application = flask.g.service.applications.get_by_sid(applications_sid)
         user.applications_id = application.applications_id
-        self.service.users.add(entity=user, by=application)
+        flask.g.service.users.add(entity=user, by=application)
 
         try:
-            self.service.commit()
+            flask.g.service.commit()
         except repositories.EntityConflict:
             http_status_code = http.HTTPStatus.CONFLICT
             message = ("""There is already an existing user with the """
                        """username "{username}".""")
-            self.logger.info(message.format(username=user.username))
+            flask.g.logger.info(message.format(username=user.username))
         else:
             http_status_code = http.HTTPStatus.CREATED
             # TODO (duyn): Log resource creation.
-
-        self.service.dispose()
 
         return body, http_status_code, headers
 
