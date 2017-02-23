be5d01c654b75c136f99a79e4d93eb8b630c77da
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-23 15:06:17 -0500

    ME-441: Added business-level logging

--- tuxedo_mask/resources/users_resource.py
+++ tuxedo_mask/resources/users_resource.py
@@ -13,9 +13,18 @@ class UsersCollectionResource(flask_restful.Resource):
 
     @authentication.login_required
     def post(self, applications_sid):
-        # TODO (duyn): Log user creation.
+        flask.g.event.update({'event_name': 'UserAdd',
+                              'event_state': 'PENDING'})
+        flask.g.logger.info('', extra=flask.g.event)
 
         user = views.UsersView().load(data=flask.request.get_json()).data
+
+        flask.g.event.update({
+            'event_state': 'STARTING',
+            'application_sid': applications_sid,
+            'user_username': user.username,
+            'user_encoded_password': flask.request.get_json()['password']})
+        flask.g.logger.info('', extra=flask.g.event)
 
         application = flask.g.service.applications.get_by_sid(applications_sid)
         user.applications_id = application.applications_id
@@ -23,7 +32,9 @@ class UsersCollectionResource(flask_restful.Resource):
         flask.g.service.commit()
 
         flask.g.http_status_code = http.HTTPStatus.CREATED
-        # TODO (duyn): Log resource creation.
+
+        flask.g.event.update({'event_state': 'COMPLETE'})
+        flask.g.logger.info('', extra=flask.g.event)
 
         return flask.g.get_response()
 
