7965929caf0f7d034a4fcd46962f433258884bc6
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-23 10:21:50 -0500

    ME-441: Changed code layout

--- tuxedo_mask/resources/applications_resource.py
+++ tuxedo_mask/resources/applications_resource.py
@@ -25,13 +25,14 @@ class ApplicationsCollectionResource(flask_restful.Resource):
         users_view = views.UsersView()
         users_view.fields['username'].load_from = 'name'
 
-        application = views.ApplicationsView().load(data=flask.request.get_json()).data
-        user = users_view.load(data=flask.request.get_json()).data
-
         tuxedo_mask_application = flask.g.service.applications.get_by_name(
             name='tuxedo_mask')
+        application = (
+            views.ApplicationsView().load(data=flask.request.get_json()).data)
+        user = users_view.load(data=flask.request.get_json()).data
         user.applications_id = tuxedo_mask_application.applications_id
-        flask.g.service.applications.add(entity=application, by=tuxedo_mask_application)
+        flask.g.service.applications.add(entity=application,
+                                         by=tuxedo_mask_application)
         flask.g.service.users.add(entity=user, by=tuxedo_mask_application)
         flask.g.service.commit()
 
