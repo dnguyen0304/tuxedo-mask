a352813ac9812182f450150781c73c7bd00198c7
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-23 13:45:36 -0500

    ME-441: Fixed missing response Location header

--- tuxedo_mask/resources/__init__.py
+++ tuxedo_mask/resources/__init__.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from .authentication import authentication
-from .applications_resource import ApplicationsCollectionResource
+from .applications_resource import (ApplicationsCollectionResource,
+                                    ApplicationsResource)
 from .login_attempts_resource import LoginAttemptsResource
 from .users_resource import UsersCollectionResource
 
 __all__ = ['ApplicationsCollectionResource',
+           'ApplicationsResource',
            'LoginAttemptsResource',
            'UsersCollectionResource',
            'authentication']
