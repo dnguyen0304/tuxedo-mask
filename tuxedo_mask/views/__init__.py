aab731839cdacc123963ec01e9e80f7e8c5d9392
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-23 12:09:27 -0500

    ME-441: Added LoginAttemptsView

--- tuxedo_mask/views/__init__.py
+++ tuxedo_mask/views/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from .base_view import BaseView
+from .login_attempts_view import LoginAttemptsView
 from .users_view import UsersView
 from .applications_view import ApplicationsView
 
-__all__ = ['ApplicationsView', 'BaseView', 'UsersView']
+__all__ = ['ApplicationsView', 'BaseView', 'LoginAttemptsView', 'UsersView']
 
