251c9d9c47d0c81f9ae9c9131a73ea6f68ae9085
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-23 10:16:33 -0500

    ME-441: Migrated to use request global

--- tuxedo_mask/resources/applications_resource.py
+++ tuxedo_mask/resources/applications_resource.py
@@ -1,23 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import collections
 import http
-import logging
 
 import flask
 import flask_restful
-import marshmallow
 
-from tuxedo_mask import repositories, services, views
+from tuxedo_mask import views
 
 
 class ApplicationsCollectionResource(flask_restful.Resource):
-
-    def __init__(self):
-        # Flask-RESTful constructs a new Resource for every request.
-        # Should these be global objects?
-        self.logger = logging.getLogger('tuxedo_mask')
-        self.service = services.TuxedoMaskService.from_configuration()
 
     # Although initially counter-intuitive, Tuxedo Mask actually
     # delegates to itself for authentication. How this works is the
@@ -31,41 +22,21 @@ class ApplicationsCollectionResource(flask_restful.Resource):
     def post(self):
         # TODO (duyn): Log application registration / sign-up.
 
-        body = collections.OrderedDict()
-        http_status_code = None
-        headers = dict()
-
-        applications_view = views.ApplicationsView()
         users_view = views.UsersView()
         users_view.fields['username'].load_from = 'name'
-        data = flask.request.get_json()
 
-        try:
-            application = applications_view.load(data=data).data
-            user = users_view.load(data=data).data
-        except marshmallow.exceptions.ValidationError as e:
-            http_status_code = http.HTTPStatus.BAD_REQUEST
-            self.logger.info(e.messages)
-            return body, http_status_code, headers
+        application = views.ApplicationsView().load(data=flask.request.get_json()).data
+        user = users_view.load(data=flask.request.get_json()).data
 
-        tuxedo_mask_application = self.service.applications.get_by_name(
+        tuxedo_mask_application = flask.g.service.applications.get_by_name(
             name='tuxedo_mask')
         user.applications_id = tuxedo_mask_application.applications_id
-        self.service.applications.add(entity=application, by=tuxedo_mask_application)
-        self.service.users.add(entity=user, by=tuxedo_mask_application)
+        flask.g.service.applications.add(entity=application, by=tuxedo_mask_application)
+        flask.g.service.users.add(entity=user, by=tuxedo_mask_application)
+        flask.g.service.commit()
 
-        try:
-            self.service.commit()
-        except repositories.EntityConflict:
-            http_status_code = http.HTTPStatus.CONFLICT
-            message = ("""There is already an existing application with the """
-                       """name "{name}".""")
-            self.logger.info(message.format(name=application.name))
-        else:
-            http_status_code = http.HTTPStatus.CREATED
-            # TODO (duyn): Log resource creation.
+        flask.g.http_status_code = http.HTTPStatus.CREATED
+        # TODO (duyn): Log resource creation.
 
-        self.service.dispose()
-
-        return body, http_status_code, headers
+        return flask.g.get_response()
 
