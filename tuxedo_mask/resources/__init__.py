69c08c8c1ba1565c98cead369d0ed41bc66bf7c8
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-23 13:45:10 -0500

    Reverted "ME-441: Fixed missing response Location header"
    This reverts commit cb70cb24722353d016061811c56f5b306070917f.

--- tuxedo_mask/resources/__init__.py
+++ tuxedo_mask/resources/__init__.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from .authentication import authentication
-from .applications_resource import (ApplicationsCollectionResource,
-                                    ApplicationsResource)
+from .applications_resource import ApplicationsCollectionResource
 from .login_attempts_resource import LoginAttemptsResource
 from .users_resource import UsersCollectionResource
 
 __all__ = ['ApplicationsCollectionResource',
-           'ApplicationsResource',
            'LoginAttemptsResource',
            'UsersCollectionResource',
            'authentication']
