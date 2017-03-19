e7131670c9dd12f697ce70543a93d9fce73b1d39
ubuntu <dnguyen0304@gmail.com>
2017-03-19 19:19:08 -0400

    N/A: Changed setup.py to use find_packages()

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ if __name__ == '__main__':
 
     description = ('A lightweight, minimalist Identity and Access Management '
                    'microservice.')
-    url = 'https://github.com/dnguyen0304/{package_name}.git'.format(
-        package_name=package_name.replace('_', '-'))
+    url = 'https://github.com/dnguyen0304/tuxedo-mask.git'
     dependency_links = [
         'git+https://github.com/dnguyen0304/python-common.git@v0.4.2#egg=common-0.4.2']
 
@@ -26,15 +25,9 @@ if __name__ == '__main__':
                      author='Duy Nguyen',
                      author_email='dnguyen0304@gmail.com',
                      license='MIT',
-                     packages=[package_name,
-                               package_name + '.models',
-                               package_name + '.repositories',
-                               package_name + '.resources',
-                               package_name + '.services',
-                               package_name + '.views'],
+                     packages=setuptools.find_packages(exclude=['*.tests']),
                      dependency_links=dependency_links,
                      install_requires=install_requires,
                      test_suite='nose.collector',
                      tests_require=['nose'],
                      include_package_data=True)
-
