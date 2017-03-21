bb1e5d4666f357d0107f641f0220e10875aa13d3
ubuntu <dnguyen0304@gmail.com>
2017-03-21 15:02:22 -0400

    ME-370: Added container for building Tuxedo Mask

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu
 
 NAMESPACE="tuxedomask"
+BRANCH=$1
 
 cd $(dirname $0)
 
@@ -16,10 +17,25 @@ mkdir build
 
 # Include uWSGI.
 docker build \
-    --file "${BUILD_ROOT}/buildtime/uwsgi/Dockerfile" \
-    --tag ${NAMESPACE}-uwsgi/buildtime:latest \
+    --file buildtime/uwsgi/Dockerfile \
+    --tag ${NAMESPACE}-uwsgi/buildtime:${BRANCH} \
     buildtime/uwsgi
-docker run --volume ${BUILD_ROOT}/build:/tmp/build ${NAMESPACE}-uwsgi/buildtime:latest
+docker run \
+    --volume ${BUILD_ROOT}/build:/tmp/build \
+    ${NAMESPACE}-uwsgi/buildtime:${BRANCH}
+
+# Include Tuxedo Mask.
+cp -r ../configuration buildtime/${NAMESPACE}
+
+docker build \
+    --file buildtime/${NAMESPACE}/Dockerfile \
+    --tag ${NAMESPACE}-${NAMESPACE}/buildtime:${BRANCH} \
+    buildtime/${NAMESPACE}
+docker run \
+    --volume ${BUILD_ROOT}/build:/tmp/build \
+    ${NAMESPACE}-${NAMESPACE}/buildtime:${BRANCH} \
+    ${BRANCH}
 
 # Tear down the build environment.
 rm -r build
+rm -r buildtime/${NAMESPACE}/configuration
---
+++ dockerfiles/buildtime/tuxedomask/build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -eu
+
+COMPONENT="tuxedomask"
+
+git clone https://github.com/dnguyen0304/tuxedo-mask.git ${COMPONENT}
+cd ${COMPONENT}
+git checkout $1
+cd ..
+
+PACKAGE="${COMPONENT}-$(grep -Po "version='\K\d\.\d\.\d" ${COMPONENT}/setup.py)"
+
+mkdir ${PACKAGE}
+
+# Include the source code.
+cp -r ${COMPONENT}/${COMPONENT} ${PACKAGE}
+
+# Include the dependencies.
+pip install --requirement requirements.txt --target ${PACKAGE}
+
+# Include the configuration.
+cp -r configuration ${PACKAGE}
+
+# Compress the package.
+cd ${PACKAGE}
+zip -9qr ${PACKAGE}.zip *
+cp ${PACKAGE}.zip ${COMPONENT}-latest.zip
+mv ${COMPONENT}-*.zip ${SHARED_VOLUME}
